28a040e12df09d1c5158c2d08b50b8d23ee9208e
Jinho <dfghcvb11@naver.com>
2023-02-12 21:05:15 +0900

    hello red crab!

---
+++ src/main/java/com/redcrab/redcrabserver/RedcrabServerApplication.java
@@ -0,0 +1,13 @@
+package com.redcrab.redcrabserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedcrabServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedcrabServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/redcrab/redcrabserver/RedcrabServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.redcrab.redcrabserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedcrabServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
